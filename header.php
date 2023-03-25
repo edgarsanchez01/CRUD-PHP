8ae09f08fb3c5a5c443bde6eda413ef8290b1e6e
edgarsanchez01 <a3521110095@alumno.uttehuacan.edu.mx>
2023-03-24 23:55:34 -0600

    segundo avance

--- header.php
+++ header.php
@@ -4,10 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Crud mongo con PHP</title>
     <link rel="stylesheet" href="./public/bootstrap5/bootstrap.min.css">
-    <link rel="stylesheet" href="./public/fontawesome-free-6.3.0-web/css/all.css">
-    <link rel="stylesheet" href="./public/css/style.css">
+    <link rel="stylesheet" href="./public/fontawesome-free-6.1.1-web/css/all.css">
+    <link rel="stylesheet" href="./public/css/estilo.css">
+    <title>Crud mongo con php</title>
 </head>
 <body>
-    
